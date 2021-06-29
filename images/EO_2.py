712b0d978d91645d91eeb15e4ded71a0d26f5376
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2021-06-29 23:32:34 +0530

    Update EO_2.py

--- images/EO_2.py
+++ images/EO_2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 27 22:19:16 2021
-
-@author: kiyisi.24
-"""
 
 def panama():
     from selenium import webdriver
@@ -18,25 +13,7 @@ def panama():
     #date_stamp = str(datetime.datetime.now()).split('.')[0]
     #date_stamp = date_stamp.replace(" ","_")
     #file_name = date_stamp + ".png"
-    driver.save_screenshot("y1.png");
-
-def texas():
-    from selenium import webdriver
-    from webdriver_manager.chrome import ChromeDriverManager
-    import pydirectinput
-    import time
-    import datetime
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get('https://www.earthcam.com/usa/texas/elpaso/?cam=elpaso');
-    #pydirectinput.moveTo(100, 100)
-    #pydirectinput.click()
-    #t_end = time.time() + 25
-    #while time.time() < t_end:
-    #    date_stamp = str(datetime.datetime.now()).split('.')[0]
-    #    date_stamp = date_stamp.replace(" ","_")
-    #    file_name = date_stamp + ".png"
-    driver.save_screenshot("eltx.png");
-
+    driver.save_screenshot("US/Panama/y1.png");
 
 def LA():
     from selenium import webdriver
@@ -50,23 +27,23 @@ def LA():
     #date_stamp = str(datetime.datetime.now()).split('.')[0]
     #date_stamp = date_stamp.replace(" ","_")
     #file_name = date_stamp + ".png"
-    driver.save_screenshot("elsl.png");
+    driver.save_screenshot("US/LA/elsl.png");
 
-def tz():
+def rome_antheon():
     from selenium import webdriver
     from webdriver_manager.chrome import ChromeDriverManager
     import pydirectinput
     import datetime
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get('https://g0.ipcamlive.com/player/player.php?alias=zkpcam&timelapseplayerenabled=0');
+    driver.get('http://www.albergodelsenato.it/webcam.php');
     #pydirectinput.moveTo(100, 100)
     #pydirectinput.click()
     #date_stamp = str(datetime.datetime.now()).split('.')[0]
     #date_stamp = date_stamp.replace(" ","_")
     #file_name = date_stamp + ".png"
-    driver.save_screenshot("ip.png");
-    
-def rome():
+    driver.save_screenshot("Italy/Rome/Rome_Piazza_Del_Pantheon.png");
+
+def rome_trevi():
     from selenium import webdriver
     from webdriver_manager.chrome import ChromeDriverManager
     import pydirectinput
@@ -78,21 +55,125 @@ def rome():
     #date_stamp = str(datetime.datetime.now()).split('.')[0]
     #date_stamp = date_stamp.replace(" ","_")
     #file_name = date_stamp + ".png"
-    driver.save_screenshot("rome.png");
+    driver.save_screenshot("Italy/Rome/Rome_Trevi_Fountain.png");
+    
+def rome_Spagna():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pydirectinput
+    import datetime
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.skylinewebcams.com/webcam/italia/lazio/roma/piazza-di-spagna.html');
+    #pydirectinput.moveTo(100, 100)
+    #pydirectinput.click()
+    #date_stamp = str(datetime.datetime.now()).split('.')[0]
+    #date_stamp = date_stamp.replace(" ","_")
+    #file_name = date_stamp + ".png"
+    driver.save_screenshot("Italy/Rome/Rome_Piazza_di_Spagna.png");
 
+def rome_il_palazzetto():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pydirectinput
+    import datetime
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.hotelhasslerroma.com/en/il-palazzetto-footer-pages/webcam');
+    #pydirectinput.moveTo(100, 100)
+    #pydirectinput.click()
+    #date_stamp = str(datetime.datetime.now()).split('.')[0]
+    #date_stamp = date_stamp.replace(" ","_")
+    #file_name = date_stamp + ".png"
+    driver.save_screenshot("Italy/Rome/Rome_Il_Palazzetto.png");
+    
+def rome_colosseum():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pydirectinput
+    import datetime
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.facebook.com/197761626911992/videos/230567388684465');
+    #pydirectinput.moveTo(100, 100)
+    #pydirectinput.click()
+    #date_stamp = str(datetime.datetime.now()).split('.')[0]
+    #date_stamp = date_stamp.replace(" ","_")
+    #file_name = date_stamp + ".png"
+    driver.save_screenshot("Italy/Rome/Rome_Colosseum.png");
 
+def houston():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pydirectinput
+    import datetime
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.webcamtaxi.com/en/usa/texas/houston-downtown.html');
+    #pydirectinput.moveTo(100, 100)
+    #pydirectinput.click()
+    #date_stamp = str(datetime.datetime.now()).split('.')[0]
+    #date_stamp = date_stamp.replace(" ","_")
+    #file_name = date_stamp + ".png"
+    driver.save_screenshot("US/Houston/Houston.png");
+    
+def LA2():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pydirectinput
+    import datetime
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.earthcam.com/usa/california/losangeles/hollywoodandvine/?cam=hollywoodandvine');
+    #pydirectinput.moveTo(100, 100)
+    #pydirectinput.click()
+    #date_stamp = str(datetime.datetime.now()).split('.')[0]
+    #date_stamp = date_stamp.replace(" ","_")
+    #file_name = date_stamp + ".png"
+    driver.save_screenshot("US/LA/LA.png");
+    
+def hollywood_blvd():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pydirectinput
+    import datetime
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.earthcam.com/usa/california/losangeles/hollywoodblvd/?cam=hollywoodblvd');
+    #pydirectinput.moveTo(100, 100)
+    #pydirectinput.click()
+    #date_stamp = str(datetime.datetime.now()).split('.')[0]
+    #date_stamp = date_stamp.replace(" ","_")
+    #file_name = date_stamp + ".png"
+    driver.save_screenshot("US/LA/hollywood_blvd.png");
+
+def venice_beach():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pydirectinput
+    import datetime
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.westland.net/beachcam/');
+    #pydirectinput.moveTo(100, 100)
+    #pydirectinput.click()
+    #date_stamp = str(datetime.datetime.now()).split('.')[0]
+    #date_stamp = date_stamp.replace(" ","_")
+    #file_name = date_stamp + ".png"
+    driver.save_screenshot("US/LA/venice_beach.png");
+    
 import schedule
 import time
 
 def job():
     print("I'm working...")
     panama()
-    texas()
     LA()
     tz()
-    rome()
+    rome_antheon()
+    rome_trevi()
+    rome_Spagna()
+    rome_il_palazzetto()
+    rome_colosseum()
+    houston()
+    LA2()
+    hollywood_blvd()
+    venice_beach()
 
-#schedule.every().day.at("01:00").do(job,'It is 01:00')
+#schedule.every().day.at("06:00").do(job,'It is 06:00')
 
 schedule.every(10).seconds.do(job)
 
